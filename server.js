c55aeba177bfb1c25d0e28e72f4f74ac241c3a4a
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-11-15 15:07:28 +0100

    Not found

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
+// express
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
+
+const dashboardController = require("./controllers/dashboard");
+const postsRouter = require("./routers/posts");
+const NotFoundMiddleware = require("./middlewares/NotFound");
+
 dotenv.config();
 
-app.use(express.static("public"));
+const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-const postsRouter = require("./routers/posts");
+app.get("/", dashboardController.index);
 app.use("/posts", postsRouter);
+
+
+app.use(NotFoundMiddleware)
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on http://localhost:${process.env.PORT}`);
